9e71736388e2e248e9b1d298716ffba3f7b531eb
Christopher Brownlee <cdbreach@gmail.com>
2019-06-13 18:58:06 -0400

    Fixed JawsDB

--- config/connection.js
+++ config/connection.js
@@ -1,9 +1,10 @@
 // Set up MySQL connection.
 const mysql = require('mysql');
+let connection;
 if(process.env.JAWSDB_URL){
-  const connection = mysql.createConnection(process.env.JAWSDB_URL)
+   connection = mysql.createConnection(process.env.JAWSDB_URL)
 }else{
-const connection = mysql.createConnection({
+ connection = mysql.createConnection({
   host: 'localhost',
   port: 8889,
   user: 'root',
